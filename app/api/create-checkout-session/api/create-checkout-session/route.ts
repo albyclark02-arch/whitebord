c797f28cd3cbd783fb386278d213d2293b497da8
Alby <School@LAPTOP-FJTV1EPS.trinityac.nsw.edu.au>
2026-09-03 15:51:11 +1000

    fix stripe api version

--- app/api/create-checkout-session/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/api/create-checkout-session/route.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-08-26.dahlia",
 });
 
 export async function POST(req: Request) {
